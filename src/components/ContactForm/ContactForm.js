2b51d1840673ac6afdf44a65abc7050e075e82a5
Yan-Brav <yan.braverman37@gmail.com>
2020-11-22 18:27:33 +0200

    2020_11_22 material ui has been added to app

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,9 +1,18 @@
+//libraries
 import React from 'react';
-import './ContactForm.css'
 import {connect} from 'react-redux';
-import {deleteContact, saveContact} from '../../store/actions';
 import {Formik, Field, Form} from 'formik';
 import {withRouter, useHistory} from 'react-router-dom';
+//@material-ui
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import UndoIcon from '@material-ui/icons/Undo';
+import DeleteIcon from '@material-ui/icons/Delete';
+import {makeStyles} from '@material-ui/core';
+//App files
+import {deleteContact, saveContact} from '../../store/actions';
+import './ContactForm.css';
+import AppTextField from './appTextField';
 
 const EMPTY_CONTACT = {
     surname: '',
@@ -11,11 +20,22 @@ const EMPTY_CONTACT = {
     phone: ''
 };
 
+const useStyles = makeStyles({
+    btn: {
+        margin: '5px'
+    },
+    text_field: {
+        margin: '5px'
+    }
+});
+
 function ContactForm({currentContact,
                      deleteContact,
                      saveContact}) {
 
     const history = useHistory();
+
+    const classes = useStyles();
 
     const onFormSubmit = (values) => {
         saveContact(values);
@@ -26,33 +46,49 @@ function ContactForm({currentContact,
         history.push('/');
     };
 
-
-    const renderForm = (props) => {
+    const renderForm = ({values}) => {
         return (
             <Form>
-                <div className='form-field'>
-                    <label htmlFor='surname'>Surname:</label>
-                    <Field name='surname'/>
-                </div>
-                {props.errors.surname && props.touched.surname
-                    ? <div className='error'>{props.errors.surname}</div>
-                    : null}
-                <div className='form-field'>
-                    <label htmlFor='name'>Name:</label>
-                    <Field name='name'/>
-                </div>
-                {props.errors.name && props.touched.name
-                    ? <div className='error'>{props.errors.name}</div>
-                    : null}
-                <div className='form-field'>
-                    <label htmlFor='phone'>Phone:</label>
-                    <Field name='phone'/>
-                </div>
-                <div>
-                    <button type='submit' className='save-btn'>Save</button>
-                    <button type='button' className='return-btn' onClick={goHome}>Return</button>
-                    {currentContact.id ? (<button type='button' className='delete-btn'
-                                                   onClick={() => deleteContact(props.values.id)}>Delete</button>)
+                <Field name='surname'>
+                    {AppTextField}
+                </Field>
+                <Field name='name'>
+                    {AppTextField}
+                </Field>
+                <Field name='phone'>
+                    {AppTextField}
+                </Field>
+                <div className='btn-group'>
+                    <Button type='submit'
+                            className='save-btn'
+                            variant='contained'
+                            color='primary'
+                            size='small'
+                            startIcon={<SaveIcon />}
+                            classes={{
+                                root: classes.btn
+                            }}>Save</Button>
+                    <Button type='button'
+                            className='return-btn'
+                            onClick={goHome}
+                            variant='contained'
+                            color='primary'
+                            size='small'
+                            startIcon={<UndoIcon/>}
+                            classes={{
+                                root: classes.btn
+                            }}>Return</Button>
+                    {currentContact.id ? (<Button
+                                            type='button'
+                                            className='delete-btn'
+                                            onClick={() => deleteContact(values.id)}
+                                            variant='contained'
+                                            color='secondary'
+                                            size='small'
+                                            startIcon={<DeleteIcon />}
+                                            classes={{
+                                                root: classes.btn
+                                            }}>Delete</Button>)
                         : ('')}
                 </div>
             </Form>
